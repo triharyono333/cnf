ed03fb7b69bc89e75307274e8db069ebe15a20e3
Poppy Gantini <rpoppy@icloud.com>
2015-08-18 11:16:26 +0700

    update popup

--- sites/all/modules/cnf_corporate/cnf_press_coverage.tpl.php
+++ sites/all/modules/cnf_corporate/cnf_press_coverage.tpl.php
@@ -17,7 +17,7 @@
 			</article>
 			<!-- modal -->
 			<div class="modal fade" id="myModal-<?php print $press->nid ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-				<div class="modal-dialog modal-lg" role="document">
+				<div class="modal-dialog" role="document">
 					<div class="modal-content">
 						<div class="modal-header">
 							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
