b9e5497d5a6376027405b9f342177944f7e85bb2
Poppy Gantini <rpoppy@icloud.com>
2015-09-01 09:33:11 +0700

    added accordion menu (static html)

--- sites/all/themes/cnf/page.tpl.php
+++ sites/all/themes/cnf/page.tpl.php
@@ -94,6 +94,45 @@ $arg0 = arg(0);
 					<?php if ($page['sidebar_first']): ?>
 							<?php print render($page['sidebar_first']); ?>
 					<?php endif; ?>
+					<!-- accordion menu -->
+					<div class="panel-group" id="category-menu" role="tablist" aria-multiselectable="true">
+					  <div class="panel panel-default">
+					    <div class="panel-heading" role="tab" id="menuOne">
+					        <a role="button" data-toggle="collapse" data-parent="#category-menu" href="#categoryMenuOne" aria-expanded="true" aria-controls="categoryMenuOne">
+					          Parent #1
+					        </a>
+					    </div>
+					    <div id="categoryMenuOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="menuOne">
+					      <div class="panel-body">
+					        <ul class="list-menus">
+					        	<li><a title="Child Menu #1" href="#">Child Menu #1</a></li>
+					        	<li><a title="Child Menu #2" href="#">Child Menu #2</a></li>
+					        	<li><a title="Child Menu #3" href="#">Child Menu #3</a></li>
+					        	<li><a title="Child Menu #4" href="#">Child Menu #4</a></li>
+					        	<li><a title="Child Menu #5" href="#">Child Menu #5</a></li>
+					        </ul>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="panel panel-default">
+					    <div class="panel-heading" role="tab" id="menuTwo">
+					        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#category-menu" href="#categoryMenuTwo" aria-expanded="false" aria-controls="categoryMenuTwo">
+					          Parent #2
+					        </a>
+					    </div>
+					    <div id="categoryMenuTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="menuTwo">
+					      <div class="panel-body">
+					        <ul class="list-menus">
+					        	<li><a title="Child Menu #1" href="#">Child Menu #1</a></li>
+					        	<li><a title="Child Menu #2" href="#">Child Menu #2</a></li>
+					        	<li><a title="Child Menu #3" href="#">Child Menu #3</a></li>
+					        	<li><a title="Child Menu #4" href="#">Child Menu #4</a></li>
+					        	<li><a title="Child Menu #5" href="#">Child Menu #5</a></li>
+					        </ul>
+					      </div>
+					    </div>
+					  </div>
+					</div>
 				</div>
 				<div id="main" class="col-md-9 col-sm-8 <?php print ($arg0 == 'store-locator')? $arg0 : '' ?>">
 					<?php print render($page['content']); ?>
